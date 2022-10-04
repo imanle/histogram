6eff435349eddfd520aa6bb4fe2cc35b280ac727
imanelbanna <82767304+imanle@users.noreply.github.com>
2022-10-04 21:27:13 +0300

    ok

--- kernel.cu
+++ kernel.cu
@@ -3,21 +3,22 @@
 #include "timer.h"
 
 __global__ void histogram_private_kernel(unsigned char* image, unsigned int* bins, unsigned int width, unsigned int height) {
-
-    // TODO
-
-
-
-
-
-
-
-
-
-
-
-
-
+     __shared__ char hist[NUM_BINS];
+     unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
+    if (i < NUM_BINS){
+        hist[threadIdx.x]=0;
+    }
+     __syncthreads();
+    
+    if(i < width * height) {
+        unsigned char b = image[i];
+        atomicAdd(&hist[b], 1);
+    }
+     __syncthreads();
+    if (i < width * height && i < NUM_BINS) {
+        unsigned char b = image[i];
+        atomicAdd(&bins[b],hist[b]);
+    }
 
 }
 
