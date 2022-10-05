3f3f681221bf8e25250c2192d9bb1b4c122a097a
imanelbanna <82767304+imanle@users.noreply.github.com>
2022-10-05 19:06:17 +0300

    ok

--- kernel.cu
+++ kernel.cu
@@ -34,16 +34,16 @@ void histogram_gpu_private(unsigned char* image_d, unsigned int* bins_d, unsigne
 __global__ void histogram_private_coarse_kernel(unsigned char* image, unsigned int* bins, unsigned int width, unsigned int height) {
 
     __shared__ int hist_s[NUM_BINS];
-     unsigned int i = blockIdx.x*blockDim.x+threadIdx.x;
+     unsigned int i = blockIdx.x*blockDim.x+threadIdx.x*COARSE_FACTOR;
      if(threadIdx.x < NUM_BINS){
           hist_s[threadIdx.x]=0;
      }
      
     __syncthreads();
-     for(unsigned int c=0 ; c<COARSE_FACTOR;++c)
+     for(unsigned int c=0; c<COARSE_FACTOR;++c)
      {
-          if(i+blockDim.x*c<width*height)
-               atomicAdd(&hist_s[image[i+blockDim.x*c]],1);
+          if((i*blockDim.x+c)<(width*height))
+               atomicAdd(&hist_s[image[i*blockDim.x+c]],1);
      }
                          
      __syncthreads();
@@ -55,8 +55,9 @@ __global__ void histogram_private_coarse_kernel(unsigned char* image, unsigned i
 
 void histogram_gpu_private_coarse(unsigned char* image_d, unsigned int* bins_d, unsigned int width, unsigned int height) {
 
-    const unsigned int numThreadsPerBlock = 1024;
-     const unsigned int numBlocks = (width*height + numThreadsPerBlock - 1)/numThreadsPerBlock;
+     unsigned int numThreadsPerBlock = 1024;
+     unsigned int numBlocks = (width*height + numThreadsPerBlock - 1)/COARSE_FACTOR*numThreadsPerBlock;
+     
     histogram_private_coarse_kernel<<< numBlocks, numThreadsPerBlock >>>(image_d,bins_d, width,height);
 
 
