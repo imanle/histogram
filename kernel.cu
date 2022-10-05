141b3d576b9731574b0df3c9c45134803086ce45
imanelbanna <82767304+imanle@users.noreply.github.com>
2022-10-05 13:39:09 +0300

    meow

--- kernel.cu
+++ kernel.cu
@@ -3,23 +3,22 @@
 #include "timer.h"
 
 __global__ void histogram_private_kernel(unsigned char* image, unsigned int* bins, unsigned int width, unsigned int height) {
-     __shared__ char hist[NUM_BINS];
+     __shared__ char hist_s [NUM_BINS];
      unsigned int i = blockIdx.x * blockDim.x + threadIdx.x;
     if (i < NUM_BINS){
-        hist[threadIdx.x]=0;
+        hist_s[threadIdx.x]=0;
     }
      __syncthreads();
     
     if(i < width * height) {
         unsigned char b = image[i];
-        atomicAdd(&hist[b], 1);
+        atomicAdd(&hist_s[b], 1);
     }
      __syncthreads();
     if (i < width * height && i < NUM_BINS) {
         unsigned char b = image[i];
-        atomicAdd(&bins[b],hist[b]);
+        atomicAdd(&bins[b],hist_s[b]);
     }
-
 }
 
 void histogram_gpu_private(unsigned char* image_d, unsigned int* bins_d, unsigned int width, unsigned int height) {
